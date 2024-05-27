TheParasiteProject/device_parasite_sepolicy

parasite: sepolicy: Update Pixel sepolicy inclusion
* Adapt to Android 14 changes

Change-Id: If3e485110f81d7740d17ee5f4d9dc878dacb5065

common/sepolicy.mk
@@ -34,6 +34,9 @@ endif
 # Pixel common
 TARGET_INCLUDE_PIXEL_SEPOLICY ?= true
 ifeq ($(TARGET_INCLUDE_PIXEL_SEPOLICY), true)
+BOARD_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/private/google
+
 # Flipendo
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/flipendo

NekoPixel/device_neko_sepolicy

Signed-off-by: Dmitrii <bankersenator@gmail.com>

common/sepolicy.mk
@@ -36,6 +36,9 @@ endif
 # Pixel common
 TARGET_INCLUDE_PIXEL_SEPOLICY ?= true
 ifeq ($(TARGET_INCLUDE_PIXEL_SEPOLICY), true)
+BOARD_SEPOLICY_DIRS += \
+    device/neko/sepolicy/common/private/google
+
 # Flipendo
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/flipendo